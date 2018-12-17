18186b13a9de9b498e020ca500655c12a37a97ed
garethosullivan <gareth.osullivan@icloud.com>
2018-12-17 18:37:25 +0000

    Initial Commit

---
+++ OnTheMapTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMapTest
+//
+//  Created by Gareth O'Sullivan on 17/12/2018.
+//  Copyright © 2018 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
