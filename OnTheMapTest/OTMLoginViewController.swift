f59a72e4c56fa18edef79f0231f28e68c9d5415a
garethosullivan <gareth.osullivan@icloud.com>
2018-12-21 20:49:25 +0000

    Added variables and constants for data task

--- OnTheMapTest/OTMLoginViewController.swift
+++ OnTheMapTest/OTMLoginViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class OTMLoginViewController: UIViewController {
 
+    let method = "https://onthemap-api.udacity.com/v1/session"
+    var request = URLRequest(url: URL(string: method))
+    let session = URLSession.shared
+    let task = session.dataTask(with: request) {data, response, error in
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
