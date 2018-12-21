183645028ce2835e6ec95945b7374d7643ffe4e6
garethosullivan <gareth.osullivan@icloud.com>
2018-12-21 21:00:17 +0000

    Removed constants and variables

--- OnTheMapTest/OTMLoginViewController.swift
+++ OnTheMapTest/OTMLoginViewController.swift
@@ -12,14 +12,9 @@ class OTMLoginViewController: UIViewController {
 
     var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
     let session = URLSession.shared
-    let task = session.dataTask(with: request) {data, response, error in
-        
-        
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 }
