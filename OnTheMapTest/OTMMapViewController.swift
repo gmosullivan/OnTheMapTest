eceee3a42be7bcfec1fc1dedafc435a5f46d6e6c
garethosullivan <gareth.osullivan@icloud.com>
2019-01-02 21:16:14 +0000

    Created getStudentLocation method to perform GET request

--- OnTheMapTest/OTMMapViewController.swift
+++ OnTheMapTest/OTMMapViewController.swift
@@ -13,5 +13,18 @@ class OTMMapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    //GET Student Locations
+    
+    func getStudentLocations() {
+        var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation?limit=100&order=updatedAt")!)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            
+        }
+        task.resume()
+    }
 
 }
