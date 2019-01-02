9fea24281b2a6bc2a96d8a574b1258e012b49ac0
garethosullivan <gareth.osullivan@icloud.com>
2019-01-02 21:01:41 +0000

    Added OTMMapViewController to download and display StudentLocations

---
+++ OnTheMapTest/OTMMapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OTMMapViewController.swift
+//  OnTheMapTest
+//
+//  Created by Gareth O'Sullivan on 02/01/2019.
+//  Copyright © 2019 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class OTMMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
