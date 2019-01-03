7fd449a1c62d82b2a7d213b0249b96267289c231
garethosullivan <gareth.osullivan@icloud.com>
2019-01-03 06:57:35 +0000

    Added StudentLocation to store latestStudentLocations

---
+++ OnTheMapTest/StudentLocation.swift
@@ -0,0 +1,9 @@
+//
+//  StudentLocation.swift
+//  OnTheMapTest
+//
+//  Created by Gareth O'Sullivan on 03/01/2019.
+//  Copyright © 2019 Locust Redemption. All rights reserved.
+//
+
+import Foundation
