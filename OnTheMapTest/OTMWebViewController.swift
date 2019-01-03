f65a83ea0dd288c6d2ccc8a7984ebafc59d9da33
garethosullivan <gareth.osullivan@icloud.com>
2019-01-03 18:22:31 +0000

    Added OTMWebViewController

---
+++ OnTheMapTest/OTMWebViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OTMWebViewController.swift
+//  OnTheMapTest
+//
+//  Created by Gareth O'Sullivan on 03/01/2019.
+//  Copyright © 2019 Locust Redemption. All rights reserved.
+//
+
+import UIKit
+
+class OTMWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
