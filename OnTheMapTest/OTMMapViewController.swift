90b1710a6479488819d6ea349136900f787dce74
garethosullivan <gareth.osullivan@icloud.com>
2019-01-07 07:17:18 +0000

    Added mapView outlet to connect mapViewDelegate

--- OnTheMapTest/OTMMapViewController.swift
+++ OnTheMapTest/OTMMapViewController.swift
@@ -14,6 +14,10 @@ class OTMMapViewController: UIViewController, MKMapViewDelegate {
     //MARK: - Variables
     var annotations = [MKPointAnnotation]()
     
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getStudentLocations()
