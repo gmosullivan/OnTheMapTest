9816bae174a44f959845b7c286670c5938d0bcd5
garethosullivan <gareth.osullivan@icloud.com>
2019-01-08 20:11:56 +0000

    Added prepare for segue function to send url to web view

--- OnTheMapTest/OTMTableViewController.swift
+++ OnTheMapTest/OTMTableViewController.swift
@@ -71,15 +71,11 @@ class OTMTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
     
     //GET Student Locations
     
