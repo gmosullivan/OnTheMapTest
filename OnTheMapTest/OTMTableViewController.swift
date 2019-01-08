1573e97d2af360de97496fb47586761d2947e348
garethosullivan <gareth.osullivan@icloud.com>
2019-01-08 19:31:32 +0000

    Copied get location functions

--- OnTheMapTest/OTMTableViewController.swift
+++ OnTheMapTest/OTMTableViewController.swift
@@ -80,5 +80,49 @@ class OTMTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    //GET Student Locations
+    
+    func getStudentLocations() {
+        var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation?limit=100&order=-updatedAt")!)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                self.displayError(error: "Something went wrong!", "Please check your network connection or try again later.")
+                return
+            }
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                self.displayError(error: "Something went wrong!", "Please check your network connection or try again later.")
+                return
+            }
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String:AnyObject]
+            } catch {
+                self.displayError(error: "Something went wrong!", "Please check your network connection or try again later.")
+                return
+            }
+            guard let results = parsedResult["results"] as? [[String:AnyObject]] else {
+                self.displayError(error: "Something went wrong!", "Please check your network connection or try again later.")
+                return
+            }
+            let locations = StudentLocation.studentLoactionsFrom(results: results)
+            print(locations)
+        }
+        task.resume()
+    }
+    
+    //MARK: - Error Functions
+    
+    func displayError(error: String, _ description: String) {
+        print(error)
+        let alert = UIAlertController(title: error, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertActionStyle.default, handler: nil))
+        performUIUpdatesOnMain {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 
 }
