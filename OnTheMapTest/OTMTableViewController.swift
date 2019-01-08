df0bfcff24abeaf7702b4dc3baf33211b6bccd63
garethosullivan <gareth.osullivan@icloud.com>
2019-01-08 18:07:40 +0000

    Deleted wrong function

--- OnTheMapTest/OTMTableViewController.swift
+++ OnTheMapTest/OTMTableViewController.swift
@@ -14,9 +14,11 @@ class OTMTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
