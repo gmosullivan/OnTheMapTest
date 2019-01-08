3e6016d327355b1588d91eacd6a3bbfbc78d8aec
garethosullivan <gareth.osullivan@icloud.com>
2019-01-08 19:38:57 +0000

    Implemented table data source methods

--- OnTheMapTest/OTMTableViewController.swift
+++ OnTheMapTest/OTMTableViewController.swift
@@ -20,12 +20,10 @@ class OTMTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return locations.count
     }
 
