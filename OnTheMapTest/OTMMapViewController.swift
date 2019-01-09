08fc638d71ad51442409eba349c8ba6f6122a6ba
garethosullivan <gareth.osullivan@icloud.com>
2019-01-09 07:31:56 +0000

    Copied error and status code checks

--- OnTheMapTest/OTMMapViewController.swift
+++ OnTheMapTest/OTMMapViewController.swift
@@ -104,29 +104,6 @@ class OTMMapViewController: UIViewController, MKMapViewDelegate {
         }
     }
     
-    /*
-     var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
-     request.httpMethod = "DELETE"
-     var xsrfCookie: HTTPCookie? = nil
-     let sharedCookieStorage = HTTPCookieStorage.shared
-     for cookie in sharedCookieStorage.cookies! {
-     if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
-     }
-     if let xsrfCookie = xsrfCookie {
-     request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
-     }
-     let session = URLSession.shared
-     let task = session.dataTask(with: request) { data, response, error in
-     if error != nil { // Handle error…
-     return
-     }
-     let range = Range(5..<data!.count)
-     let newData = data?.subdata(in: range) /* subset response data! */
-     print(String(data: newData!, encoding: .utf8)!)
-     }
-     task.resume()
-    */
-    
     @IBAction func logout() {
         var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
         request.httpMethod = "DELETE"
@@ -140,7 +117,14 @@ class OTMMapViewController: UIViewController, MKMapViewDelegate {
         }
         let session = URLSession.shared
         let task = session.dataTask(with: request) { data, response, error in
-            
+            guard error == nil else {
+                self.displayError(error: "Something went wrong!", "Please check your network connection or try again later.")
+                return
+            }
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                self.displayError(error: "Something went wrong!", "Please check your network connection or try again later.")
+                return
+            }
             
         }
         task.resume()
