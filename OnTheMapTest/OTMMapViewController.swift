2b12c6a5497a07471c139a4d68c27577d29bb048
garethosullivan <gareth.osullivan@icloud.com>
2019-01-09 07:18:24 +0000

    Added request to store properties for post request

--- OnTheMapTest/OTMMapViewController.swift
+++ OnTheMapTest/OTMMapViewController.swift
@@ -104,4 +104,33 @@ class OTMMapViewController: UIViewController, MKMapViewDelegate {
         }
     }
     
+    /*
+     var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
+     request.httpMethod = "DELETE"
+     var xsrfCookie: HTTPCookie? = nil
+     let sharedCookieStorage = HTTPCookieStorage.shared
+     for cookie in sharedCookieStorage.cookies! {
+     if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+     }
+     if let xsrfCookie = xsrfCookie {
+     request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+     }
+     let session = URLSession.shared
+     let task = session.dataTask(with: request) { data, response, error in
+     if error != nil { // Handle error…
+     return
+     }
+     let range = Range(5..<data!.count)
+     let newData = data?.subdata(in: range) /* subset response data! */
+     print(String(data: newData!, encoding: .utf8)!)
+     }
+     task.resume()
+    */
+    
+    @IBAction func logout() {
+        var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/session")!)
+        request.httpMethod = "DELETE"
+        
+    }
+    
 }
