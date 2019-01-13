cab133c2dd3486a6726975d5e8372240f1a08b99
garethosullivan <gareth.osullivan@icloud.com>
2019-01-13 09:25:06 +0100

    Testing changes

--- OnTheMapTest/OTMMapViewController.swift
+++ OnTheMapTest/OTMMapViewController.swift
@@ -118,11 +118,11 @@ class OTMMapViewController: UIViewController, MKMapViewDelegate {
         let session = URLSession.shared
         let task = session.dataTask(with: request) { data, response, error in
             guard error == nil else {
-                self.displayError(error: "Something went wrong!", "Unable to logout. Please try again later.")
+                self.displayError(error: "Something went wrong!1", "Unable to logout. Please try again later.")
                 return
             }
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
-                self.displayError(error: "Something went wrong!", "Unable to logout. Please try again later.")
+                self.displayError(error: "Something went wrong!2", "Unable to logout. Please try again later.")
                 return
             }
             let range = Range(5..<data!.count)
@@ -131,15 +131,17 @@ class OTMMapViewController: UIViewController, MKMapViewDelegate {
             do {
                 parsedResult = try JSONSerialization.jsonObject(with: result!, options: .allowFragments) as! [String:AnyObject]
             } catch {
-                self.displayError(error: "Something went wrong!", "Unable to logout. Please try again later.")
+                self.displayError(error: "Something went wrong!3", "Unable to logout. Please try again later.")
                 return
             }
-            guard let session = parsedResult["session"] as? [[String:Any]] else {
-                self.displayError(error: "Something went wrong!", "Unable to logout. Please try again later.")
+            guard let session = parsedResult["session"] as? [String:Any] else {
+                self.displayError(error: "Something went wrong!4", "Unable to logout. Please try again later.")
                 return
             }
             if session.count > 0 {
-                self.dismiss(animated: true)
+                performUIUpdatesOnMain {
+                    self.dismiss(animated: true)
+                }
             }
         }
         task.resume()
